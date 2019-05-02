c0b30fafa8f78fe3eff18163cb3b55e771e454cd
Ernesto <eceescalona@gmail.com>
2019-05-01 22:37:45 -0400

    Base de datos 2

---
+++ SeppimCaraibesApp/Data/ORM/QuotesView.cs
@@ -0,0 +1,103 @@
+namespace SeppimCaraibesApp.Data.ORM
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Data.Entity.Spatial;
+
+    [Table("QuotesView")]
+    public partial class QuotesView
+    {
+        [Key]
+        [Column("Order Code", Order = 0)]
+        [StringLength(50)]
+        public string Order_Code { get; set; }
+
+        [Column(TypeName = "date")]
+        public DateTime? Date { get; set; }
+
+        [Column("Offer Period")]
+        public int? Offer_Period { get; set; }
+
+        [Column("Provider Reference")]
+        [StringLength(50)]
+        public string Provider_Reference { get; set; }
+
+        [Column("Provider Name")]
+        [StringLength(250)]
+        public string Provider_Name { get; set; }
+
+        [Column("Provider Phone")]
+        [StringLength(50)]
+        public string Provider_Phone { get; set; }
+
+        [Column("Customer Reference")]
+        [StringLength(50)]
+        public string Customer_Reference { get; set; }
+
+        [Column("Customer Name")]
+        [StringLength(250)]
+        public string Customer_Name { get; set; }
+
+        [Column("Customer Phone")]
+        [StringLength(50)]
+        public string Customer_Phone { get; set; }
+
+        [Column("Customer Address")]
+        public string Customer_Address { get; set; }
+
+        [Column("Payment Option")]
+        public byte? Payment_Option { get; set; }
+
+        [Column("Shipping Method")]
+        public byte? Shipping_Method { get; set; }
+
+        public byte? Devise { get; set; }
+
+        [StringLength(250)]
+        public string Incoterm { get; set; }
+
+        [Key]
+        [Column("Product Code", Order = 1)]
+        [StringLength(50)]
+        public string Product_Code { get; set; }
+
+        [Column("Product Name")]
+        [StringLength(250)]
+        public string Product_Name { get; set; }
+
+        [Column("Product Qty")]
+        public int? Product_Qty { get; set; }
+
+        [Column("Unit Price")]
+        public decimal? Unit_Price { get; set; }
+
+        public decimal? Discount { get; set; }
+
+        public decimal? EXW { get; set; }
+
+        public decimal? Freight { get; set; }
+
+        [Column("CPT-CFR")]
+        public byte? CPT_CFR { get; set; }
+
+        [Column("Total Cost")]
+        public double? Total_Cost { get; set; }
+
+        [Column("Gross Weight")]
+        public decimal? Gross_Weight { get; set; }
+
+        [Column("Net Weight")]
+        public decimal? Net_Weight { get; set; }
+
+        [Key]
+        [Column(Order = 2)]
+        [StringLength(50)]
+        public string Packing { get; set; }
+
+        [Column("Place of Departure")]
+        [StringLength(250)]
+        public string Place_of_Departure { get; set; }
+    }
+}
---
+++ SeppimCaraibesApp/Data/ORM/SeppimCaraibesLocalEntities.cs
@@ -0,0 +1,214 @@
+namespace SeppimCaraibesApp.Data.ORM
+{
+    using System;
+    using System.Data.Entity;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Linq;
+
+    public partial class SeppimCaraibesLocalEntities : DbContext
+    {
+        public SeppimCaraibesLocalEntities()
+            : base("name=SeppimCaraibesLocalEntities")
+        {
+        }
+
+        public virtual DbSet<Bank> Banks { get; set; }
+        public virtual DbSet<Customer> Customers { get; set; }
+        public virtual DbSet<Order> Orders { get; set; }
+        public virtual DbSet<Origin> Origins { get; set; }
+        public virtual DbSet<Product> Products { get; set; }
+        public virtual DbSet<ProductsOrder> ProductsOrders { get; set; }
+        public virtual DbSet<Provider> Providers { get; set; }
+        public virtual DbSet<Shipment> Shipments { get; set; }
+        public virtual DbSet<CustomersView> CustomersViews { get; set; }
+        public virtual DbSet<InvoicesView> InvoicesViews { get; set; }
+        public virtual DbSet<OrdersView> OrdersViews { get; set; }
+        public virtual DbSet<PreOrdersView> PreOrdersViews { get; set; }
+        public virtual DbSet<ProductsView> ProductsViews { get; set; }
+        public virtual DbSet<ProvidersView> ProvidersViews { get; set; }
+        public virtual DbSet<QuotesView> QuotesViews { get; set; }
+        public virtual DbSet<ShipmentsView> ShipmentsViews { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Order>()
+                .Property(e => e.ContractDescription)
+                .IsUnicode(false);
+
+            modelBuilder.Entity<Order>()
+                .Property(e => e.DocRequired)
+                .IsUnicode(false);
+
+            modelBuilder.Entity<Order>()
+                .Property(e => e.Observations)
+                .IsUnicode(false);
+
+            modelBuilder.Entity<Order>()
+                .Property(e => e.EXW)
+                .HasPrecision(18, 4);
+
+            modelBuilder.Entity<Order>()
+                .Property(e => e.Freight)
+                .HasPrecision(18, 4);
+
+            modelBuilder.Entity<Order>()
+                .Property(e => e.FCA)
+                .HasPrecision(18, 4);
+
+            modelBuilder.Entity<Order>()
+                .Property(e => e.FOB)
+                .HasPrecision(18, 4);
+
+            modelBuilder.Entity<Order>()
+                .Property(e => e.Insurance)
+                .HasPrecision(18, 4);
+
+            modelBuilder.Entity<Order>()
+                .Property(e => e.Inspection)
+                .HasPrecision(18, 4);
+
+            modelBuilder.Entity<Order>()
+                .HasOptional(e => e.Shipment)
+                .WithRequired(e => e.Order)
+                .WillCascadeOnDelete();
+
+            modelBuilder.Entity<Origin>()
+                .Property(e => e.Acronyms)
+                .IsFixedLength();
+
+            modelBuilder.Entity<Origin>()
+                .HasMany(e => e.Products)
+                .WithMany(e => e.Origins)
+                .Map(m => m.ToTable("ProductsOrigins").MapLeftKey("OriginId").MapRightKey("ProductId"));
+
+            modelBuilder.Entity<Product>()
+                .Property(e => e.UnitPrice)
+                .HasPrecision(18, 4);
+
+            modelBuilder.Entity<Product>()
+                .Property(e => e.QtyPrice)
+                .HasPrecision(18, 4);
+
+            modelBuilder.Entity<Product>()
+                .Property(e => e.SalePrice)
+                .HasPrecision(18, 4);
+
+            modelBuilder.Entity<Product>()
+                .HasMany(e => e.Providers)
+                .WithMany(e => e.Products)
+                .Map(m => m.ToTable("ProductsProviders").MapLeftKey("ProductId").MapRightKey("ProviderId"));
+
+            modelBuilder.Entity<ProductsOrder>()
+                .Property(e => e.Discount)
+                .HasPrecision(18, 4);
+
+            modelBuilder.Entity<ProductsOrder>()
+                .Property(e => e.Interests)
+                .HasPrecision(18, 4);
+
+            modelBuilder.Entity<Shipment>()
+                .Property(e => e.GrossWeight)
+                .HasPrecision(18, 4);
+
+            modelBuilder.Entity<Shipment>()
+                .Property(e => e.NetWeight)
+                .HasPrecision(18, 4);
+
+            modelBuilder.Entity<InvoicesView>()
+                .Property(e => e.Contract_Description)
+                .IsUnicode(false);
+
+            modelBuilder.Entity<InvoicesView>()
+                .Property(e => e.Product_Interests)
+                .HasPrecision(18, 4);
+
+            modelBuilder.Entity<InvoicesView>()
+                .Property(e => e.Product_Discount)
+                .HasPrecision(18, 4);
+
+            modelBuilder.Entity<InvoicesView>()
+                .Property(e => e.Freight)
+                .HasPrecision(18, 4);
+
+            modelBuilder.Entity<InvoicesView>()
+                .Property(e => e.FCA)
+                .HasPrecision(18, 4);
+
+            modelBuilder.Entity<InvoicesView>()
+                .Property(e => e.FOB)
+                .HasPrecision(18, 4);
+
+            modelBuilder.Entity<InvoicesView>()
+                .Property(e => e.Insurance)
+                .HasPrecision(18, 4);
+
+            modelBuilder.Entity<InvoicesView>()
+                .Property(e => e.Inspection)
+                .HasPrecision(18, 4);
+
+            modelBuilder.Entity<OrdersView>()
+                .Property(e => e.Unit_Price)
+                .HasPrecision(18, 4);
+
+            modelBuilder.Entity<OrdersView>()
+                .Property(e => e.Product_Discount)
+                .HasPrecision(18, 4);
+
+            modelBuilder.Entity<OrdersView>()
+                .Property(e => e.Contract_Description)
+                .IsUnicode(false);
+
+            modelBuilder.Entity<OrdersView>()
+                .Property(e => e.Documents_Required)
+                .IsUnicode(false);
+
+            modelBuilder.Entity<ProductsView>()
+                .Property(e => e.Unit_Price)
+                .HasPrecision(18, 4);
+
+            modelBuilder.Entity<ProductsView>()
+                .Property(e => e.Qty_Price)
+                .HasPrecision(18, 4);
+
+            modelBuilder.Entity<ProductsView>()
+                .Property(e => e.Sale_Price)
+                .HasPrecision(18, 4);
+
+            modelBuilder.Entity<ProductsView>()
+                .Property(e => e.Acronyms)
+                .IsFixedLength();
+
+            modelBuilder.Entity<QuotesView>()
+                .Property(e => e.Unit_Price)
+                .HasPrecision(18, 4);
+
+            modelBuilder.Entity<QuotesView>()
+                .Property(e => e.Discount)
+                .HasPrecision(18, 4);
+
+            modelBuilder.Entity<QuotesView>()
+                .Property(e => e.EXW)
+                .HasPrecision(18, 4);
+
+            modelBuilder.Entity<QuotesView>()
+                .Property(e => e.Freight)
+                .HasPrecision(18, 4);
+
+            modelBuilder.Entity<QuotesView>()
+                .Property(e => e.Gross_Weight)
+                .HasPrecision(18, 4);
+
+            modelBuilder.Entity<QuotesView>()
+                .Property(e => e.Net_Weight)
+                .HasPrecision(18, 4);
+
+            modelBuilder.Entity<ShipmentsView>()
+                .Property(e => e.Gross_Weight)
+                .HasPrecision(18, 4);
+
+            modelBuilder.Entity<ShipmentsView>()
+                .Property(e => e.Net_Weight)
+                .HasPrecision(18, 4);
+        }
+    }
+}
