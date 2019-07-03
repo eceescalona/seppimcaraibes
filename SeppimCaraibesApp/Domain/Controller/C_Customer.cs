25d7b69132342462f19b913d4b20a7575c6be9b9
Ernesto Carmona <eceescalona@gmail.com>
2019-07-03 15:22:28 -0400

    Add GetEnumValueFromDescription in Controllers

--- SeppimCaraibesApp/Domain/Controller/C_Customer.cs
+++ SeppimCaraibesApp/Domain/Controller/C_Customer.cs
@@ -90,6 +90,29 @@
             }
         }
 
+        public T GetValueFromDescription<T>(string description)
+        {
+            var type = typeof(T);
+
+            if (!type.IsEnum) throw new InvalidOperationException();
+            foreach (var field in type.GetFields())
+            {
+                var attribute = Attribute.GetCustomAttribute(field,
+                    typeof(DescriptionAttribute)) as DescriptionAttribute;
+                if (attribute != null)
+                {
+                    if (attribute.Description == description)
+                        return (T)field.GetValue(null);
+                }
+                else
+                {
+                    if (field.Name == description)
+                        return (T)field.GetValue(null);
+                }
+            }
+            throw new ArgumentException("Not found.", "description");
+        }
+
 
         #region CustomerManage
         public void AddCustomer(IAddEditCustomer addEditCustomer, Data.ORM.Customer customer)
--- SeppimCaraibesApp/Domain/Controller/C_Order.cs
+++ SeppimCaraibesApp/Domain/Controller/C_Order.cs
@@ -144,6 +144,29 @@
             }
         }
 
+        public T GetValueFromDescription<T>(string description)
+        {
+            var type = typeof(T);
+
+            if (!type.IsEnum) throw new InvalidOperationException();
+            foreach (var field in type.GetFields())
+            {
+                var attribute = Attribute.GetCustomAttribute(field,
+                    typeof(DescriptionAttribute)) as DescriptionAttribute;
+                if (attribute != null)
+                {
+                    if (attribute.Description == description)
+                        return (T)field.GetValue(null);
+                }
+                else
+                {
+                    if (field.Name == description)
+                        return (T)field.GetValue(null);
+                }
+            }
+            throw new ArgumentException("Not found.", "description");
+        }
+
         public async void LoadQuoteReport(IReport reportQuote, string code)
         {
             var reportData = await _mOrder.GetOrderReportView(_context, code);
