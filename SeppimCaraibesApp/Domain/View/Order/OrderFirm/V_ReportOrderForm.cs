c5ed9797bc2d2d837beb7c956d69c268c51f7097
Ernesto Carmona <eceescalona@gmail.com>
2019-10-13 21:28:31 -0400

    Add logs into Order

--- SeppimCaraibesApp/Domain/View/Order/OrderFirm/V_ReportOrderForm.cs
+++ SeppimCaraibesApp/Domain/View/Order/OrderFirm/V_ReportOrderForm.cs
@@ -1,21 +1,24 @@
 ﻿namespace SeppimCaraibesApp.Domain.View.Order
 {
+    using SeppimCaraibesApp.Domain.Controller;
+    using System;
     using System.Windows.Forms;
 
     internal partial class V_ReportOrderForm : Form
     {
         private const string NAME = "Vista Previa Orden";
 
-        private readonly Controller.C_Order _cOrder;
+        private readonly C_Order _cOrder;
 
 
+        #region Ctor
         public V_ReportOrderForm()
         {
             InitializeComponent();
-            _cOrder = new Controller.C_Order();
+            _cOrder = new C_Order();
         }
 
-        public V_ReportOrderForm(Controller.C_Order cOrder, string code)
+        public V_ReportOrderForm(C_Order cOrder, string code)
         {
             InitializeComponent();
             _cOrder = cOrder;
@@ -25,7 +28,7 @@
             LoadDocumentSource(code, true);
         }
 
-        public V_ReportOrderForm(Controller.C_Order cOrder, string code, bool flag)
+        public V_ReportOrderForm(C_Order cOrder, string code, bool flag)
         {
             InitializeComponent();
             _cOrder = cOrder;
@@ -34,19 +37,28 @@
 
             LoadDocumentSource(code, flag);
         }
+        #endregion
 
 
         private void LoadDocumentSource(string code, bool flag)
         {
-            if (flag)
+            try
             {
-                var report = new Reports.Order.R_Order(_cOrder, code);
-                orderDV.DocumentSource = report;
+                if (flag)
+                {
+                    var report = new Reports.Order.R_Order(_cOrder, code);
+                    orderDV.DocumentSource = report;
+                }
+                else
+                {
+                    var report = new Reports.Order.R_Order(_cOrder, code, flag);
+                    orderDV.DocumentSource = report;
+                }
             }
-            else
+            catch (Exception ex)
             {
-                var report = new Reports.Order.R_Order(_cOrder, code, flag);
-                orderDV.DocumentSource = report;
+                C_Log _cLog = new C_Log();
+                _cLog.Write(ex.Message, ETypeOfMessage.Error);
             }
         }
     }
