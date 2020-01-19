09bbd35bff30c6765e23eabecb75322187b0c896
Ernesto Carmona <eceescalona@gmail.com>
2020-01-19 13:11:25 -0500

    change logic to sale price view

--- SeppimCaraibesApp/Domain/View/Order/Pre-Order/V_AddEditPreOrderForm.cs
+++ SeppimCaraibesApp/Domain/View/Order/Pre-Order/V_AddEditPreOrderForm.cs
@@ -364,10 +364,7 @@
                 {
                     if (indexsProducts[i] != -1)
                     {
-                        var product = (Data.POCO.ProductsOrders)productsGV.GetRow(indexsProducts[i]);
-                        var salePrice = product.UnitPrice / product.SalePrice * 100;
-                        product.SalePrice = salePrice;
-                        products.Add(product);
+                        products.Add((Data.POCO.ProductsOrders)productsGV.GetRow(indexsProducts[i]));
                     }
                 }
 
